1cf85456ac47537fd0d48d4d1acbcab99a3b72db
Tyler Bates <Fondlytyler@gmail.com>
2020-05-05 14:04:55 -0700

    add ai gameplay.

--- HigherLower.cs
+++ HigherLower.cs
@@ -26,10 +26,50 @@ public class Guess
        HighLow(min,max);
     };    
   }
+
+  public static void LowerHigher(int myNumber)
+  {
+    
+    Console.WriteLine("Guess jabronie.");
+    int response = int.Parse(Console.ReadLine());
+    if (response < myNumber)
+    {
+      Console.WriteLine("Higher");
+      LowerHigher(myNumber);
+    }
+    if (response > myNumber)
+    {
+      Console.WriteLine("Lower");
+      LowerHigher(myNumber);
+    }
+    if (response == myNumber)
+    {
+      Console.WriteLine("yeah yeah, don't let it go to your head.");
+      
+    }
+  }
   public static void Main()
 {
   Console.WriteLine("Let's play a game of Higher/Lower");
+  Console.WriteLine("Who should go first? [YOU or ME]");
+  string gameType = Console.ReadLine().ToLower();
+  if(gameType == "you")
+  {
+     Random rand = new Random();
+    int myNumber = rand.Next(1, 101);
+    LowerHigher(myNumber);
+  }
+ else if (gameType == "me")
+  {
   HighLow(0,101);
-}
+  }
+  else
+  { 
+    Console.WriteLine("wise guy huh? Fine I'll go then.");
+     Random rand = new Random();
+    int myNumber = rand.Next(1, 101);
+    LowerHigher(myNumber);
+    }
+  }
 }
 
